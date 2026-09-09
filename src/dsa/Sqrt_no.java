85f7ebceb6763b990a5b24dea4476a85fb290ad4
bharat1914 <bharat.j10110@gmail.com>
2026-09-09 16:04:00 +0530

    sqrt of number using binary search

--- src/dsa/Sqrt_no.java
+++ src/dsa/Sqrt_no.java
@@ -4,7 +4,26 @@ public class Sqrt_no {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int t= 25;
+		int l=0;
+		int r=t;
+		int c = -1;
 		
+		while(l<=r) {
+			int mid = (l+r)/2;
+			if(mid*mid==t) {
+				c=mid;
+				break;
+			}
+			else if(mid*mid<t) {
+				l=mid+1;
+			}
+			
+			else {
+				r=mid-1;  
+			}
+		}
+		System.out.println(c);
 	}
 
 }
